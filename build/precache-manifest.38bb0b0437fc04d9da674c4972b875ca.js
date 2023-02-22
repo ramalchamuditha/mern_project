fee5e78f5b47012a96918ab9e66291516f04087a
Ramal Chamuditha <36332204+ramalchamuditha@users.noreply.github.com>
2023-02-22 23:09:56 +0530

    Add front-end files with build folder

---
+++ build/precache-manifest.38bb0b0437fc04d9da674c4972b875ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1092092f5c8d32b34dfdb3ccbd99640a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7aa26365f3fd366b758",
+    "url": "/static/css/main.81fb1591.chunk.css"
+  },
+  {
+    "revision": "a6b7bf14cd99c32f8a34",
+    "url": "/static/js/2.061ce9de.chunk.js"
+  },
+  {
+    "revision": "6a3fd5f48a97c7577e9c3ede94b4a5b5",
+    "url": "/static/js/2.061ce9de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7aa26365f3fd366b758",
+    "url": "/static/js/main.684ad328.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.684ad328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
